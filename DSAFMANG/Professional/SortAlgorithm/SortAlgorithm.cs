ce71d224a45671ccb1b47cadc225730a12aa3054
Sonu Kumar <sonusheohar15@gmail.com>
2026-05-10 03:23:09 +0530

    Learn BruteAlgorithm

---
+++ DSAFMANG/Professional/SortAlgorithm/SortAlgorithm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAFMANG.Professional.SortAlgorithm
+{
+    public class SortAlgorithm
+    {
+        public int[] SortingByBruteAlgorithm(int[] arr)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int minValue=arr[i];
+                int minIndex=i;
+                for(int j=i+1; j<arr.Length; j++)
+                {
+                    if (arr[j]<minValue)
+                    {
+                        minValue = arr[j];
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+    }
+}
--- DSAFMANG/Program.cs
+++ DSAFMANG/Program.cs
@@ -1,3 +1,10 @@
 ﻿using System;
+using DSAFMANG.Professional.SortAlgorithm;
+int[] arr = { 64, 25, 12, 22, 11 };
 
+Console.WriteLine("Original Array: " + string.Join(", ", arr));
+
+SortAlgorithm sortAlgorithm = new SortAlgorithm();
+var arrayResult=sortAlgorithm.SortingByBruteAlgorithm(arr);
+Console.WriteLine("Sorted Array: " + string.Join(", ", arrayResult));
 Console.ReadLine();
