5756de5074f8ac9e085a42988952d663c8701fc1
Sonu Kumar <sonusheohar15@gmail.com>
2026-05-11 04:50:50 +0530

    Implement Selection Sort Algorithm

--- DSAFMANG/Professional/SortAlgorithm/SortAlgorithm.cs
+++ DSAFMANG/Professional/SortAlgorithm/SortAlgorithm.cs
@@ -53,6 +53,59 @@ namespace DSAFMANG.Professional.SortAlgorithm
             }
             return arr;
         }
+
+        /// <summary>
+        /// Selection Sort is a simple comparison-based sorting algorithm. 
+        /// It works by repeatedly finding the minimum element from the 
+        /// unsorted portion of the array and placing it at the beginning
+        /// 
+        /// How Selection Sort Works
+        /// 	Outer Loop → Start from the first element and move through the array.
+        /// 	Find Minimum → In each pass, search the smallest element in the remaining unsorted part.
+        /// 	Swap Operation → Swap that minimum element with the first element of the unsorted part.
+        /// 	Repeat Process → Continue until all elements are sorted.
+        /// 	Result → Sorted array in ascending order.
+        /// 
+        /// Step-by-Step Explanation
+        /// 	Start Outer Loop
+        /// o	Begin at the first element of the array.
+        /// o	This loop runs n−1 times (where n is the number of elements).
+        /// 
+        /// 	Find Minimum
+        /// o	In each pass, assume the current element is the minimum.
+        /// o	Compare it with the rest of the unsorted elements.
+        /// o	Update the index if a smaller element is found.
+        /// 
+        /// 	Swap Operation
+        /// o	Swap the smallest element found with the first element of the unsorted portion.
+        /// o	After the swap, the first element of that pass is now in its correct position.
+        /// 
+        /// 	Repeat Process
+        /// o	Move to the next position and repeat the process until all elements are sorted.
+        /// o	At the end of all passes, the array is sorted in ascending order.
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int[] SelectionSortAlgorithm(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for(int j=i+1; j<n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                var temp= arr[minIndex];
+                arr[minIndex]=arr[i];
+                arr[i]=temp;
+            }
+            return arr;
+        }
         public int[] SortingByBruteAlgorithm(int[] arr)
         {
             for (int i = 0; i < arr.Length-1; i++)
--- DSAFMANG/Program.cs
+++ DSAFMANG/Program.cs
@@ -5,6 +5,6 @@ int[] arr = { 64, 25, 12, 22, 11 };
 Console.WriteLine("Original Array: " + string.Join(", ", arr));
 
 SortAlgorithm sortAlgorithm = new SortAlgorithm();
-var arrayResult=sortAlgorithm.BubbleSortAlgorithm(arr);
+var arrayResult=sortAlgorithm.SelectionSortAlgorithm(arr);
 Console.WriteLine("Sorted Array: " + string.Join(", ", arrayResult));
 Console.ReadLine();
